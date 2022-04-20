6a7289448667cb17255060dc9ef4151d6cb0f085
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-04-20 18:01:11 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <Header />
+    <Main />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Main,
+  },
+  data() {
+    return {
+      apiURL: "https://api.themoviedb.org/3/search/",
+      apiKey: "cb32b76d65a06b66bff56fd2212a7853",
+      movies: [],
+      series: [],
+      query: "",
+      searching: false,
+    };
+  },
+  mounted() {
+    axios
+      .get(this.apiURL + "tv")
+      .then(({ data }) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <h1>Main</h1>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
+
+<style scoped lang="scss">
+main {
+  height: calc(100% - 100px);
+}
+</style>
