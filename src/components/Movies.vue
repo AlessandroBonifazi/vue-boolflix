a1621820cffd6eb8c842089e2488626773adc082
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-04-21 12:27:42 +0200

    adding series and movies components

---
+++ src/components/Movies.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div v-for="(movie, index) in movies" :key="index" class="card">
+      <img :src="imgURL + movie.poster_path" :alt="movie.title" />
+      <h4>{{ movie.title }}</h4>
+      <p>{{ movie.original_title }}</p>
+      <p>{{ movie.original_language }}</p>
+      <p>{{ movie.vote_average }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MoviesList",
+  props: {
+    movies: Array,
+    imgURL: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  width: 80%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  .card {
+    margin: 20px;
+    padding: 10px;
+    border: 2px solid black;
+  }
+}
+</style>
---
+++ src/components/Series.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div v-for="(serie, index) in series" :key="index" class="card">
+      <img :src="imgURL + serie.poster_path" :alt="serie.name" />
+
+      <h4>{{ serie.name }}</h4>
+      <p>{{ serie.original_name }}</p>
+      <p>{{ serie.original_language }}</p>
+      <p>{{ serie.vote_average }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SeriesList",
+  props: {
+    series: Array,
+    imgURL: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  width: 80%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  .card {
+    margin: 20px;
+    padding: 10px;
+    border: 2px solid black;
+  }
+}
+</style>
