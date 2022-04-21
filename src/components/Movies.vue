0a7518bd3555d60fcb6a0f2f3c7afac188ea8e7d
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-04-21 12:59:51 +0200

    changing vote to star

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -3,9 +3,9 @@
     <div v-for="(movie, index) in movies" :key="index" class="card">
       <img :src="imgURL + movie.poster_path" :alt="movie.title" />
       <h4>{{ movie.title }}</h4>
-      <p>{{ movie.original_title }}</p>
-      <p>{{ movie.original_language }}</p>
-      <p>{{ movie.vote_average }}</p>
+      <p>Titolo originale: {{ movie.original_title }}</p>
+      <p>Lingua: {{ movie.original_language }}</p>
+      <p>Voto: {{ voteToStar(movie.vote_average) }}</p>
     </div>
   </div>
 </template>
@@ -17,19 +17,29 @@ export default {
     movies: Array,
     imgURL: String,
   },
+  methods: {
+    voteToStar(x) {
+      const vote = Math.ceil(x / 2);
+      return vote;
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
 .container {
   width: 80%;
+  margin: auto;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   .card {
-    margin: 20px;
-    padding: 10px;
-    border: 2px solid black;
+    display: flex;
+    flex-direction: column;
+    align-content: space-between;
+    width: 25%;
+    margin: 10px;
+    padding: 5px;
   }
 }
 </style>
--- src/components/Series.vue
+++ src/components/Series.vue
@@ -4,9 +4,9 @@
       <img :src="imgURL + serie.poster_path" :alt="serie.name" />
 
       <h4>{{ serie.name }}</h4>
-      <p>{{ serie.original_name }}</p>
-      <p>{{ serie.original_language }}</p>
-      <p>{{ serie.vote_average }}</p>
+      <p>Titolo originale:{{ serie.original_name }}</p>
+      <p>Lingua: {{ serie.original_language }}</p>
+      <p>Voto: {{ voteToStar(serie.vote_average) }}</p>
     </div>
   </div>
 </template>
@@ -18,19 +18,29 @@ export default {
     series: Array,
     imgURL: String,
   },
+  methods: {
+    voteToStar(x) {
+      const vote = Math.ceil(x / 2);
+      return vote;
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
 .container {
   width: 80%;
+  margin: auto;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   .card {
-    margin: 20px;
-    padding: 10px;
-    border: 2px solid black;
+    display: flex;
+    flex-direction: column;
+    align-content: space-between;
+    width: 25%;
+    margin: 10px;
+    padding: 5px;
   }
 }
 </style>
